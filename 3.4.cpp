9ebf598430b766041e28793ff156677abacb005c
CRASHFIRE-666 <54841369+CRASHFIRE-666@users.noreply.github.com>
2019-12-10 15:10:19 +0300

    Create 3.4.cpp

---
+++ 3.4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    int x;
+    cout<<"Введите число"<<endl;
+    cin>>x;
+    cout<<"Y="<<(3*pow(x,6))-(6*pow(x,2))-7<<endl;
+}
