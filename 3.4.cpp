26891d3decad0195d3b3bdd93007a63a194ee48b
CRASHFIRE666 <54841369+CRASHFIRE-666@users.noreply.github.com>
2019-12-24 22:42:41 +0300

    Update 3.4.cpp

--- 3.4.cpp
+++ 3.4.cpp
@@ -4,8 +4,11 @@ using namespace std;
 
 int main()
 {
+    setlocale(LC_ALL, "Russian");
     int x;
     cout<<"Введите число"<<endl;
     cin>>x;
     cout<<"Y="<<(3*pow(x,6))-(6*pow(x,2))-7<<endl;
+    system("pause");
+    return 0;
 }
