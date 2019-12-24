b074f4313bdd06753261ed3f7bf355e97c1c07e7
CRASHFIRE666 <54841369+CRASHFIRE-666@users.noreply.github.com>
2019-12-24 22:43:58 +0300

    Update 3.7.cpp

--- 3.7.cpp
+++ 3.7.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main()
 {
+    setlocale(LC_ALL, "Russian");
     float a1,a2,a3;
     cout<<"Введите число"<<endl;
     cin>>a1;
@@ -12,5 +13,6 @@ int main()
     a1=a3*a3;
     a2=a1*a3;
     cout<<a2<<endl;
+    system("pause");
     return 0;
 }
